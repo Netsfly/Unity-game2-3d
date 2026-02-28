9146460404d73c52ffb6ea37fcc08aab66b384fd
Netsfly <sasha.tsoi20@gmail.com>
2026-02-28 20:24:00 +0600

    new wasd

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; // оставляем только UnityEngine
+
+public class GameInput : MonoBehaviour
+{
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = new Vector2(0, 0); // UnityEngine.Vector2
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
