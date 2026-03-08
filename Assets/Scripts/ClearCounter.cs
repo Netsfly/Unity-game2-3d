f53b40764df7dd38e831b9b05c5451c685035215
Netsfly <sasha.tsoi20@gmail.com>
2026-03-08 20:26:48 +0600

    update

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ClearCounter : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Interacting with ClearCounter");
+    }
+
+
+
+}
